9b166220d599096c87a8ca0f66d56a4d7891f871
PranavKocharlakota <pranav.kocharlakota@gmail.com>
2025-10-07 15:26:52 -0700

    Added Sensor task and queue
    Remove default functions, implement SensorTask and helper functions, and add queue for inter-task communication.

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,67 @@
 #include <FreeRTOS.h>
 #include <task.h>
 #include <Arduino.h>
+#include <queue.h>
 
 
-
-// put function declarations here:
+//Function declarations: 
 int myFunction(int, int);
 
+//Global Variables:
+QueueHandle_t testQueue;
+float sensorBuffer[5];
+int bufferIndex = 0;
+int bufferCount = 0;
 
-
+struct dataPack{
+  float value;
+  int error;
+};
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
+//Function definitions:
 int myFunction(int x, int y) {
   return x + y;
 }
 
-void vLowPriorityTask(void *pvParameters){
-  for(;;){
-    Serial.println("Low Priority!");
-  }
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-}
-void vHighPriorityTask(void *pvParameters){
-  for(;;){
-    printf("High Priority!");
-  }
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
+float generateSensorSignal(){
+  return random(-10, 111); 
 }
 
+float movingAverage(float newValue){
+  sensorBuffer[bufferIndex] = newValue;
+  bufferIndex = (bufferIndex + 1) % 5;
+  if (bufferCount < 5) {bufferCount++;}
+  float sum = 0;
+  for (int i = 0; i < bufferCount; i++){
+    sum += sensorBuffer[i];
+  }
+  printf("%d" , sum/bufferCount);
+  return sum/bufferCount;
+}
 
+void vSensorTask(void *pvParameters){
+  for(;;){
+    float sensorValue = generateSensorSignal();
+    float filteredValue = movingAverage(sensorValue);
+    struct dataPack d1;
+    d1.value = filteredValue;
+    if (filteredValue < 0 || filteredValue > 100){
+      d1.error = 1;
+    }
+    xQueueSend(testQueue, &d1, portMAX_DELAY);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  xTaskCreate(vLowPriorityTask, "Low", 1000, NULL, 1, NULL);
-  xTaskCreate(vHighPriorityTask, "High", 1000, NULL, 2, NULL);
+  testQueue = xQueueCreate(5, sizeof(struct dataPack));
+  xTaskCreate(vSensorTask, "Sensor", 1000, NULL, 1, NULL);
   vTaskStartScheduler();
+
 }
