48d684a5f8112913106e450491db1af1623b8e89
PranavKocharlakota <pranav.kocharlakota@gmail.com>
2025-10-07 12:40:20 -0700

    Platformio env changed. Two test tasks created.
    Changed Platformio.ini environment from native to teensy. Created two test tasks with different priority levels.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 #include <stdio.h>
-//#include <FreeRTOS.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <Arduino.h>
+
+
 
 // put function declarations here:
 int myFunction(int, int);
 
 
-int main() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  printf("%d", result);
-  return 0;
-}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -21,3 +20,25 @@ int myFunction(int x, int y) {
   return x + y;
 }
 
+void vLowPriorityTask(void *pvParameters){
+  for(;;){
+    Serial.println("Low Priority!");
+  }
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
+void vHighPriorityTask(void *pvParameters){
+  for(;;){
+    printf("High Priority!");
+  }
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  xTaskCreate(vLowPriorityTask, "Low", 1000, NULL, 1, NULL);
+  xTaskCreate(vHighPriorityTask, "High", 1000, NULL, 2, NULL);
+  vTaskStartScheduler();
+}
